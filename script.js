18dffed635d7f13474a9b6f3d5f713b3518bc6bb
Joshua <joshuaakintemi3@gmail.com>
2026-09-01 18:51:45 +0100

    head done

---
+++ script.js
@@ -0,0 +1,14 @@
+const den = document.getElementById("den");
+const head = den.getContext("2d");
+const windowWidth = window.innerWidth;
+const windowHeight = window.innerHeight;
+
+den.width = windowWidth * 0.85;
+den.height = windowHeight * 0.85;
+
+head.beginPath();
+head.arc(den.width / 2, den.height / 2, 10, 0, Math.PI * 2);
+head.fillStyle = "rgb(22, 23, 24)";
+head.fill();
+head.strokeStyle = "white";
+head.stroke();
