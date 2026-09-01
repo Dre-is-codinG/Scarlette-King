4462f2a0737b3778248401b55e63abcf4e5a1d37
Joshua <joshuaakintemi3@gmail.com>
2026-09-01 19:11:05 +0100

    neck and head established

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 const den = document.getElementById("den");
 const head = den.getContext("2d");
-const ctx = den.getContext("2d");
+const spine = den.getContext("2d");
 const windowWidth = window.innerWidth;
 const windowHeight = window.innerHeight;
 
 den.width = windowWidth * 0.85;
 den.height = windowHeight * 0.85;
 
-ctx.beginPath();
-ctx.moveTo(100, 100);
-ctx.bezierCurveTo(150, 50, 200, 150, 250, 100);
-ctx.stroke();
+
 
 den.addEventListener("mousemove", (e) => {
+    // head
     head.clearRect(0, 0, den.width, den.height);
     head.beginPath();
     head.arc(e.offsetX + 20, e.offsetY + 20, 10, 0, Math.PI * 2);
@@ -20,4 +18,9 @@ den.addEventListener("mousemove", (e) => {
     head.fill();
     head.strokeStyle = "white";
     head.stroke();
+    // spine
+    spine.beginPath();
+    spine.moveTo(e.offsetX + 20, e.offsetY + 100);
+    spine.bezierCurveTo(e.offsetX + 150, e.offsetY + 50, e.offsetX + 200, e.offsetY + 150, e.offsetX + 250, e.offsetY + 100);
+    spine.stroke();
 })
