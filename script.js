f604ac9ec3e843b8b5a37cb5044aebc6fcc55e71
Joshua <joshuaakintemi3@gmail.com>
2026-09-01 19:06:04 +0100

    getting the spine

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const den = document.getElementById("den");
 const head = den.getContext("2d");
+const ctx = den.getContext("2d");
 const windowWidth = window.innerWidth;
 const windowHeight = window.innerHeight;
 
 den.width = windowWidth * 0.85;
 den.height = windowHeight * 0.85;
 
-head.beginPath();
-head.arc(den.width / 2, den.height / 2, 10, 0, Math.PI * 2);
-head.fillStyle = "rgb(22, 23, 24)";
-head.fill();
-head.strokeStyle = "white";
-head.stroke();
+ctx.beginPath();
+ctx.moveTo(100, 100);
+ctx.bezierCurveTo(150, 50, 200, 150, 250, 100);
+ctx.stroke();
 
 den.addEventListener("mousemove", (e) => {
     head.clearRect(0, 0, den.width, den.height);
