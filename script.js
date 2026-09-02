7500e6173d275848dc6bec7a057b7fc9ce5719fd
Joshua <joshuaakintemi3@gmail.com>
2026-09-02 11:32:08 +0100

    head and spine connected

--- script.js
+++ script.js
@@ -6,21 +6,35 @@ const windowHeight = window.innerHeight;
 
 den.width = windowWidth * 0.85;
 den.height = windowHeight * 0.85;
-
+var radius = 10;
+var gapBetween = 40
 
 
 den.addEventListener("mousemove", (e) => {
+    let distance = Math.sin(Date.now() * 0.001) * 50;
+    let start = { x: e.offsetX + gapBetween, y: e.offsetY + gapBetween };
+    
     // head
-    head.clearRect(0, 0, den.width, den.height);
-    head.beginPath();
-    head.arc(e.offsetX + 20, e.offsetY + 20, 10, 0, Math.PI * 2);
-    head.fillStyle = "rgb(22, 23, 24)";
-    head.fill();
-    head.strokeStyle = "white";
-    head.stroke();
-    // spine
-    spine.beginPath();
-    spine.moveTo(e.offsetX + 20, e.offsetY + 100);
-    spine.bezierCurveTo(e.offsetX + 150, e.offsetY + 50, e.offsetX + 200, e.offsetY + 150, e.offsetX + 250, e.offsetY + 100);
-    spine.stroke();
+    setTimeout(() => {
+        head.clearRect(0, 0, den.width, den.height);
+        head.beginPath();
+        head.arc(start.x, start.y, radius, 0, Math.PI * 2);
+        head.fillStyle = "rgb(22, 23, 24)";
+        head.fill();
+        head.strokeStyle = "white";
+        head.stroke();
+        spine.beginPath();
+        spine.moveTo(start.x, start.y);
+        spine.bezierCurveTo(
+            e.offsetX + 150 + distance,
+            e.offsetY + 50,
+            e.offsetX + 200 - distance,
+            e.offsetY + 150,
+            e.offsetX + 250,
+            e.offsetY + 100
+        );
+        spine.strokeStyle = "white";
+        spine.stroke();
+    }, 1000);
+    
 })
